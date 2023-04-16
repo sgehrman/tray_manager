98a1627426c1cdaa57da0011403bd7f5efa1e307
Steve Gehrman <sgehrman@gmail.com>
2023-04-15 22:44:35 -0700

    ff

--- linux/tray_manager_plugin.cc
+++ linux/tray_manager_plugin.cc
@@ -126,8 +126,8 @@ static FlMethodResponse* set_icon(TrayManagerPlugin* self, FlValue* args) {
 
   // we are setting the same path again, but that doesn't trigger a rebuild
   // so we set the third parameter to random to trigger a rebuild
+  // NOTE: doesn't seem to work?
   // app_indicator_set_icon(indicator, icon_path);
-  app_indicator_set_icon_full(indicator, "/wat.png", id);
   app_indicator_set_icon_full(indicator, icon_path, id);
 
   return FL_METHOD_RESPONSE(
