66d227f7c971b678e08dfa7052cb1c847a340f1d
Steve Gehrman <sgehrman@gmail.com>
2025-08-26 20:04:58 -0700

    sdfsdf

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -22,7 +22,7 @@ class _MyAppState extends State<MyApp> {
         primaryColor: Color(0xff416ff4),
         canvasColor: Colors.white,
         scaffoldBackgroundColor: Color(0xffF7F9FB),
-        dividerColor: Colors.grey.withOpacity(0.3),
+        dividerColor: Colors.grey.withValues(alpha: 0.3),
       ),
       builder: BotToastInit(),
       navigatorObservers: [BotToastNavigatorObserver()],
